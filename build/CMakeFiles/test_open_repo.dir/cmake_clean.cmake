a684f7baf1d42a357b18eac569aa845ee196b6a0
Maksym Kvitka <mkvitka@Mac.lan>
2026-02-19 00:45:09 -0800

    my message

---
+++ build/CMakeFiles/test_open_repo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_open_repo.dir/main.cpp.o"
+  "CMakeFiles/test_open_repo.dir/main.cpp.o.d"
+  "test_open_repo"
+  "test_open_repo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_open_repo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
